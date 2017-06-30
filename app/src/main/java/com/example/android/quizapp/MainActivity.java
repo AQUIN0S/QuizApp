ecddb4fae4a2cec81e63397373a11b45c6dfe268
AQUIN0S <aquinos@acyonix.com>
2017-06-30 23:39:09 +1200

    App now changes the question heading colours based on correct or wrong answers, and a toast pops up if the answers aren't all selected. Checkbox question management isn't quite as elegant as I'd like yet, I may work on that next.

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.android.quizapp;
 
+import android.support.annotation.ColorInt;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.CheckBox;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -34,25 +36,41 @@ import butterknife.OnClick;
  */
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.answer_1) RadioGroup Answer1;
-    @BindView(R.id.answer_2) RadioGroup Answer2;
-    @BindView(R.id.answer_3a) CheckBox Answer3a;
-    @BindView(R.id.answer_3b) CheckBox Answer3b;
-    @BindView(R.id.answer_3c) CheckBox Answer3c;
-    @BindView(R.id.answer_3d) CheckBox Answer3d;
-    @BindView(R.id.answer_4) RadioGroup Answer4;
-    @BindView(R.id.answer_5) RadioGroup Answer5;
-    @BindView(R.id.answer_6) RadioGroup Answer6;
+    @BindView(R.id.answer_1) RadioGroup answer1;
+    @BindView(R.id.answer_2) RadioGroup answer2;
+    @BindView(R.id.answer_3a) CheckBox answer3A;
+    @BindView(R.id.answer_3b) CheckBox answer3B;
+    @BindView(R.id.answer_3c) CheckBox answer3C;
+    @BindView(R.id.answer_3d) CheckBox answer3D;
+    @BindView(R.id.answer_4) RadioGroup answer4;
+    @BindView(R.id.answer_5) RadioGroup answer5;
+    @BindView(R.id.answer_6) RadioGroup answer6;
     @BindView(R.id.answer_1b) RadioButton correctAnswer1;
     @BindView(R.id.answer_2c) RadioButton correctAnswer2;
     @BindView(R.id.answer_4d) RadioButton correctAnswer4;
     @BindView(R.id.answer_5d) RadioButton correctAnswer5;
     @BindView(R.id.answer_6a) RadioButton correctAnswer6;
+    @BindView(R.id.question_1_heading) TextView question1Heading;
+    @BindView(R.id.question_2_heading) TextView question2Heading;
+    @BindView(R.id.question_3_heading) TextView question3Heading;
+    @BindView(R.id.question_4_heading) TextView question4Heading;
+    @BindView(R.id.question_5_heading) TextView question5Heading;
+    @BindView(R.id.question_6_heading) TextView question6Heading;
 
+    @ColorInt int correctAnswerColour = 0xFF81C784;
+    @ColorInt int wrongAnswerColour = 0xFFE57373;
+
+    // Create the ArrayLists that will be used throughout this program. If any questions and answers
+    // are added, the above BindViews and the setArrayList methods at the bottom of this program are
+    // the areas that will need to be edited. Make sure all ArrayLists are the correct length and
+    // contain the correct views and values.
+    // If new CheckBox questions are made, the process is a little more complex however. I may or
+    // may not edit this at a later date.
     private ArrayList<RadioGroup> radioGroupAnswerGroups = new ArrayList<>();
     private ArrayList<RadioButton> radioGroupCorrectAnswers = new ArrayList<>();
     private ArrayList<CheckBox> question3CheckBoxAnswers = new ArrayList<>();
     private ArrayList<Boolean> question3CorrectCheckBoxAnswers = new ArrayList<>();
+    private ArrayList<TextView> radioGroupQuestionHeadings = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,18 +79,30 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         setRadioGroupAnswerGroups(new ArrayList<RadioGroup>());
         setRadioGroupCorrectAnswers(new ArrayList<RadioButton>());
-        setQuestion3CheckBoxAnswers(new ArrayList<CheckBox>());
-        setQuestion3CorrectCheckBoxAnswers(new ArrayList<Boolean>());
+        setQ3CheckBoxAnswers(new ArrayList<CheckBox>());
+        setQ3CorrectCheckBoxAnswers(new ArrayList<Boolean>());
+        setRadioGroupQuestionHeadings(new ArrayList<TextView>());
     }
 
     @OnClick(R.id.submit_button)
     public void submitAnswers() {
-
         if (checkAllQuestionsAnswered()) {
-            boolean[] RGCorrectAnswers = checkRadioGroupCorrectAnswers();
-            boolean[] CBCorrectAnswers = checkCheckBoxCorrectAnswers();
+            ArrayList<Boolean> RGCorrectAnswers = checkRadioGroupCorrectAnswers();
+            ArrayList<Boolean> CBCorrectAnswers = checkQ3CheckBoxCorrectAnswers();
+            for (int i = 0; i < radioGroupQuestionHeadings.size(); i += 1) {
+                if (RGCorrectAnswers.get(i)) {
+                    radioGroupQuestionHeadings.get(i).setBackgroundColor(correctAnswerColour);
+                } else {
+                    radioGroupQuestionHeadings.get(i).setBackgroundColor(wrongAnswerColour);
+                }
+            }
+            if (CBCorrectAnswers.contains(false)) {
+                question3Heading.setBackgroundColor(wrongAnswerColour);
+            } else {
+                question3Heading.setBackgroundColor(correctAnswerColour);
+            }
         } else {
-            Toast toast = Toast.makeText(this, R.string.NotAllAnswered, Toast.LENGTH_SHORT);
+            Toast toast = Toast.makeText(this, R.string.NotAllAnswered, Toast.LENGTH_LONG);
             toast.show();
         }
     }
@@ -91,28 +121,44 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    private boolean[] checkRadioGroupCorrectAnswers() {
-        boolean[] RGCorrectAnswers = new boolean[radioGroupAnswerGroups.size()];
+    /**
+     * Check which answers are correct and which are wrong among the RadioGroups.
+     *
+     * @return an array of booleans that tell whether their corresponding answer is correct (true)
+     * or wrong (false).
+     */
+    private ArrayList<Boolean> checkRadioGroupCorrectAnswers() {
+        ArrayList<Boolean> RGCorrectAnswers = new ArrayList<>();
         for (int i = 0; i < radioGroupAnswerGroups.size(); i += 1) {
-            RGCorrectAnswers[i] = (radioGroupAnswerGroups.get(i).getCheckedRadioButtonId() == radioGroupCorrectAnswers.get(i).getId());
+            RGCorrectAnswers.add(radioGroupAnswerGroups.get(i).getCheckedRadioButtonId() == radioGroupCorrectAnswers.get(i).getId());
         }
         return RGCorrectAnswers;
     }
 
-    private boolean[] checkCheckBoxCorrectAnswers() {
-        boolean[] CBCorrectAnswers = new boolean[question3CheckBoxAnswers.size()];
+    /**
+     * Check which of the answers among the CheckBoxes are correct.
+     *
+     * @return an array of booleans that tell whether their corresponding CheckBox in the third
+     * question is correct (true) or wrong (false).
+     */
+    private ArrayList<Boolean> checkQ3CheckBoxCorrectAnswers() {
+        ArrayList<Boolean> CBCorrectAnswers = new ArrayList<>();
         for (int i = 0; i < question3CheckBoxAnswers.size(); i += 1) {
-            CBCorrectAnswers[i] = (question3CheckBoxAnswers.get(i).isChecked() == question3CorrectCheckBoxAnswers.get(i));
+            CBCorrectAnswers.add(question3CheckBoxAnswers.get(i).isChecked() == question3CorrectCheckBoxAnswers.get(i));
         }
         return CBCorrectAnswers;
     }
 
+    /**
+     * Set all of the ArrayLists.
+     */
+
     public void setRadioGroupAnswerGroups(ArrayList<RadioGroup> radioGroupAnswerGroups) {
-        radioGroupAnswerGroups.add(Answer1);
-        radioGroupAnswerGroups.add(Answer2);
-        radioGroupAnswerGroups.add(Answer4);
-        radioGroupAnswerGroups.add(Answer5);
-        radioGroupAnswerGroups.add(Answer6);
+        radioGroupAnswerGroups.add(answer1);
+        radioGroupAnswerGroups.add(answer2);
+        radioGroupAnswerGroups.add(answer4);
+        radioGroupAnswerGroups.add(answer5);
+        radioGroupAnswerGroups.add(answer6);
         this.radioGroupAnswerGroups = radioGroupAnswerGroups;
     }
 
@@ -125,20 +171,31 @@ public class MainActivity extends AppCompatActivity {
         this.radioGroupCorrectAnswers = radioGroupCorrectAnswers;
     }
 
-    public void setQuestion3CheckBoxAnswers(ArrayList<CheckBox> question3CheckBoxAnswers) {
-        question3CheckBoxAnswers.add(Answer3a);
-        question3CheckBoxAnswers.add(Answer3b);
-        question3CheckBoxAnswers.add(Answer3c);
-        question3CheckBoxAnswers.add(Answer3d);
+    public void setQ3CheckBoxAnswers(ArrayList<CheckBox> question3CheckBoxAnswers) {
+        question3CheckBoxAnswers.add(answer3A);
+        question3CheckBoxAnswers.add(answer3B);
+        question3CheckBoxAnswers.add(answer3C);
+        question3CheckBoxAnswers.add(answer3D);
         this.question3CheckBoxAnswers = question3CheckBoxAnswers;
     }
 
-    public void setQuestion3CorrectCheckBoxAnswers(ArrayList<Boolean> question3CorrectCheckBoxAnswers) {
+    // This one could be just a normal array, but I've decided to make it an ArrayList for
+    // consistency.
+    public void setQ3CorrectCheckBoxAnswers(ArrayList<Boolean> question3CorrectCheckBoxAnswers) {
         question3CorrectCheckBoxAnswers.add(true);
         question3CorrectCheckBoxAnswers.add(false);
         question3CorrectCheckBoxAnswers.add(true);
         question3CorrectCheckBoxAnswers.add(false);
         this.question3CorrectCheckBoxAnswers = question3CorrectCheckBoxAnswers;
+    }
+
+    public void setRadioGroupQuestionHeadings(ArrayList<TextView> radioGroupQuestionHeadings) {
+        radioGroupQuestionHeadings.add(question1Heading);
+        radioGroupQuestionHeadings.add(question2Heading);
+        radioGroupQuestionHeadings.add(question4Heading);
+        radioGroupQuestionHeadings.add(question5Heading);
+        radioGroupQuestionHeadings.add(question6Heading);
+        this.radioGroupQuestionHeadings = radioGroupQuestionHeadings;
     }
 
 }
