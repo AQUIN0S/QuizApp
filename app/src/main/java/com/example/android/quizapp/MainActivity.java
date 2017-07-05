15dd0bd8bb086fbe12507241d812e58374b5eddb
AQUIN0S <aquinos@acyonix.com>
2017-07-05 18:28:48 +1200

    Changed question 5 to no longer be duplicated, and replaced it with an EditText answer. Took out the comment that says this app will move onto a separate screen, and instead changed the toast slightly to congratulate the user if all questions are answered correctly. Also documented all methods more correctly.

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.annotation.ColorInt;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -18,21 +19,17 @@ import butterknife.OnClick;
 /**
  * Welcome to this Ancient Mythology quiz app! This app is to quickly test your knowledge on the old
  * stories, just asking a few questions about the mythology of various cultures.
- * <p>
+ *
  * How the app works:
  * When the "Submit" button is pressed, the app goes ahead and first
- * checks if all the answers are answered (checking radial button groups only, as checkbox questions
- * could potentially have no right answer listed). If an answer is not checked, then a toast
- * message will prompt the user to answer all questions.
- * <p>
+ * checks if all the answers are answered (checking radial button groups and EditTexts only, as
+ * checkbox questions could potentially have no right answer listed). If an answer is not checked,
+ * then a toast message will prompt the user to answer all questions.
+ *
  * Once all questions have been answered, the app will then check which question is right. For
  * every question answered correctly, it will display the question heading as green. Every question
  * answered wrong will be displayed as red. A toast message will also pop up letting the user know
  * their score, and prompting them to answer any wrong questions if there are.
- * <p>
- * Finally, once the user has answered every question correctly and pressed "Submit", a new window
- * will be shown, which displays the congratulations page. This page is the final place for them,
- * they passed!
  */
 public class MainActivity extends AppCompatActivity {
 
@@ -43,12 +40,11 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.answer_3c) CheckBox answer3C;
     @BindView(R.id.answer_3d) CheckBox answer3D;
     @BindView(R.id.answer_4) RadioGroup answer4;
-    @BindView(R.id.answer_5) RadioGroup answer5;
     @BindView(R.id.answer_6) RadioGroup answer6;
+    @BindView(R.id.answer_5) EditText answer5;
     @BindView(R.id.answer_1b) RadioButton correctAnswer1;
     @BindView(R.id.answer_2c) RadioButton correctAnswer2;
     @BindView(R.id.answer_4d) RadioButton correctAnswer4;
-    @BindView(R.id.answer_5d) RadioButton correctAnswer5;
     @BindView(R.id.answer_6a) RadioButton correctAnswer6;
     @BindView(R.id.question_1_heading) TextView question1Heading;
     @BindView(R.id.question_2_heading) TextView question2Heading;
@@ -72,8 +68,7 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<Boolean> question3CorrectCheckBoxAnswers = new ArrayList<>();
     private ArrayList<TextView> radioGroupQuestionHeadings = new ArrayList<>();
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
@@ -84,34 +79,14 @@ public class MainActivity extends AppCompatActivity {
         setRadioGroupQuestionHeadings(new ArrayList<TextView>());
     }
 
+    /**
+     * Called when the submit button is clicked. Checks that all questions are answered. If so, then
+     * displays the user's score. If not, prompts the user to answer all questions.
+     */
     @OnClick(R.id.submit_button)
     public void submitAnswers() {
         if (checkAllQuestionsAnswered()) {
-            double score = 0.0;
-            ArrayList<Boolean> RGCorrectAnswers = checkRadioGroupCorrectAnswers();
-            ArrayList<Boolean> CBCorrectAnswers = checkQ3CheckBoxCorrectAnswers();
-            for (int i = 0; i < radioGroupQuestionHeadings.size(); i += 1) {
-                if (RGCorrectAnswers.get(i)) {
-                    radioGroupQuestionHeadings.get(i).setBackgroundColor(correctAnswerColour);
-                    score += 1.0;
-                } else {
-                    radioGroupQuestionHeadings.get(i).setBackgroundColor(wrongAnswerColour);
-                }
-            }
-            if (CBCorrectAnswers.contains(false)) {
-                question3Heading.setBackgroundColor(wrongAnswerColour);
-            } else {
-                question3Heading.setBackgroundColor(correctAnswerColour);
-            }
-            for (boolean CorrectAnswer : CBCorrectAnswers) {
-                if (CorrectAnswer) {
-                    score += 0.25;
-                }
-            }
-            String message = getResources().getString(R.string.ScoreMessage) + " " + score +
-                    getResources().getString(R.string.QuestionTotal);
-            Toast toast = Toast.makeText(this, message, Toast.LENGTH_LONG);
-            toast.show();
+            displayScore();
         } else {
             Toast toast = Toast.makeText(this, R.string.NotAllAnswered, Toast.LENGTH_LONG);
             toast.show();
@@ -119,8 +94,54 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * Check whether all of the questions that have RadioButton answers have been answered.
-     *
+     * Display the score of the user.
+     */
+    private void displayScore() {
+        double score = 0.0;
+        String correctAnswer5 = getResources().getString(R.string.Answer5);
+        ArrayList<Boolean> RGCorrectAnswers = checkRadioGroupCorrectAnswers();
+        ArrayList<Boolean> CBCorrectAnswers = checkQ3CheckBoxCorrectAnswers();
+        for (int i = 0; i < radioGroupQuestionHeadings.size(); i += 1) {
+            if (RGCorrectAnswers.get(i)) {
+                radioGroupQuestionHeadings.get(i).setBackgroundColor(correctAnswerColour);
+                score += 1.0;
+            } else {
+                radioGroupQuestionHeadings.get(i).setBackgroundColor(wrongAnswerColour);
+            }
+        }
+        if (answer5.getText().toString().toLowerCase().replace(" ", "").equals(correctAnswer5.toLowerCase())) {
+            question5Heading.setBackgroundColor(correctAnswerColour);
+            score += 1;
+        } else {
+            question5Heading.setBackgroundColor(wrongAnswerColour);
+        }
+        if (CBCorrectAnswers.contains(false)) {
+            question3Heading.setBackgroundColor(wrongAnswerColour);
+        } else {
+            question3Heading.setBackgroundColor(correctAnswerColour);
+        }
+        for (boolean CorrectAnswer : CBCorrectAnswers) {
+            if (CorrectAnswer) {
+                score += 0.25;
+            }
+        }
+        String message;
+        if ((score >= (Double.valueOf(getResources().getString(R.string.QuestionTotal)) - 0.1))
+                && score <= (Double.valueOf(getResources().getString(R.string.QuestionTotal)) + 0.1)) {
+            message = getResources().getString(R.string.ScoreMessage) + " " + score + "/" +
+                    getResources().getString(R.string.QuestionTotal) + ", Congratulations!";
+        } else {
+            message = getResources().getString(R.string.ScoreMessage) + " " + score + "/" +
+                    getResources().getString(R.string.QuestionTotal);
+        }
+        getResources().getString(R.string.QuestionTotal);
+        Toast toast = Toast.makeText(this, message, Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    /**
+     * Check whether all of the questions that have RadioButton and EditText answers have been
+     * answered.
      * @return either true or false.
      */
     private boolean checkAllQuestionsAnswered() {
@@ -129,12 +150,11 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         }
-        return true;
+        return (answer5.getText().length() != 0);
     }
 
     /**
      * Check which answers are correct and which are wrong among the RadioGroups.
-     *
      * @return an array of booleans that tell whether their corresponding answer is correct (true)
      * or wrong (false).
      */
@@ -148,7 +168,6 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * Check which of the answers among the CheckBoxes are correct.
-     *
      * @return an array of booleans that tell whether their corresponding CheckBox in the third
      * question is correct (true) or wrong (false).
      */
@@ -161,27 +180,39 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * Set all of the ArrayLists.
+     * Set the RadioGroupAnswerGroups ArrayList.
+     * @param radioGroupAnswerGroups An array of the radioGroups in this app. This array is in a
+     *                               particular order to interact with the other ArrayLists in this
+     *                               program.
      */
-
     public void setRadioGroupAnswerGroups(ArrayList<RadioGroup> radioGroupAnswerGroups) {
         radioGroupAnswerGroups.add(answer1);
         radioGroupAnswerGroups.add(answer2);
         radioGroupAnswerGroups.add(answer4);
-        radioGroupAnswerGroups.add(answer5);
         radioGroupAnswerGroups.add(answer6);
         this.radioGroupAnswerGroups = radioGroupAnswerGroups;
     }
 
+    /**
+     * Set the RadioGroupCorrectAnswers ArrayList.
+     * @param radioGroupCorrectAnswers An array of the RadioButtons that correspond to correct
+     *                                 answers. This array is in a particular order to interact with
+     *                                 the other ArrayLists in this program.
+     */
     private void setRadioGroupCorrectAnswers(ArrayList<RadioButton> radioGroupCorrectAnswers) {
         radioGroupCorrectAnswers.add(correctAnswer1);
         radioGroupCorrectAnswers.add(correctAnswer2);
         radioGroupCorrectAnswers.add(correctAnswer4);
-        radioGroupCorrectAnswers.add(correctAnswer5);
         radioGroupCorrectAnswers.add(correctAnswer6);
         this.radioGroupCorrectAnswers = radioGroupCorrectAnswers;
     }
 
+    /**
+     * Set the Q3CheckBoxAnswers ArrayList.
+     * @param question3CheckBoxAnswers An ArrayList of the CheckBoxes in question 3. This array is
+     *                                 in a particular order to interact with the other ArrayLists
+     *                                 in this program.
+     */
     public void setQ3CheckBoxAnswers(ArrayList<CheckBox> question3CheckBoxAnswers) {
         question3CheckBoxAnswers.add(answer3A);
         question3CheckBoxAnswers.add(answer3B);
@@ -190,8 +221,14 @@ public class MainActivity extends AppCompatActivity {
         this.question3CheckBoxAnswers = question3CheckBoxAnswers;
     }
 
-    // This one could be just a normal array, but I've decided to make it an ArrayList for
-    // consistency.
+    /**
+     * Set the Q3CorrectCheckBoxAnswers ArrayList. This could be a normal array but is an ArrayList
+     * for consistency.
+     * @param question3CorrectCheckBoxAnswers An array of booleans that correspond to the correct
+     *                                        answers of question 3. This array is in a particular
+     *                                        order to interact with the other ArrayLists in this
+     *                                        program.
+     */
     public void setQ3CorrectCheckBoxAnswers(ArrayList<Boolean> question3CorrectCheckBoxAnswers) {
         question3CorrectCheckBoxAnswers.add(true);
         question3CorrectCheckBoxAnswers.add(false);
@@ -200,11 +237,17 @@ public class MainActivity extends AppCompatActivity {
         this.question3CorrectCheckBoxAnswers = question3CorrectCheckBoxAnswers;
     }
 
+    /**
+     * Set the RadioGroupQuestionHeadings ArrayList.
+     * @param radioGroupQuestionHeadings An array of TextViews that correspond to the headings of
+     *                                   the questions that contain a RadioGroup. This array is in a
+     *                                   particular order to interact with the other ArrayLists in
+     *                                   this program.
+     */
     public void setRadioGroupQuestionHeadings(ArrayList<TextView> radioGroupQuestionHeadings) {
         radioGroupQuestionHeadings.add(question1Heading);
         radioGroupQuestionHeadings.add(question2Heading);
         radioGroupQuestionHeadings.add(question4Heading);
-        radioGroupQuestionHeadings.add(question5Heading);
         radioGroupQuestionHeadings.add(question6Heading);
         this.radioGroupQuestionHeadings = radioGroupQuestionHeadings;
     }
